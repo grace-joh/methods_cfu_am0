73d8ebf2b132dc25caed7175f883c443a25256b1
Grace Joh <graceehjoh@gmail.com>
2023-01-05 12:26:16 -0600

    Update completed define_and_id.rb

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,27 @@
 
 
 "Hello World".include?("Hello")
-
+# The include? method is called on the string object "Hello World" to evaluate to a boolean value 
+# whether if the string argument "Hello" is included in the string object it's been called on. 
+# The return value is true
 
 "Hello World".end_with?("Hello")
-
+# The end_with? method is called on the string object "Hello World" to evaluate to a boolean value 
+# whether if the string object ends with the string argument "Hello". 
+# The return value is false
 
 "Hello World".end_with?("rld")
-
+# The end_with? method is called on the string object "Hello World" to evaluate to a boolean value 
+# whether if the string object ends with the string argument "rld". 
+# The return value is true
 
 12.even?
-
+# The even? method is called on the integer object 12 to evaluate to a boolean value 
+# whether if the integer object is an even number. 
+# The return value is true
 
 18.next
-
+# The next method is called on the integer object 18 and returns the next integer value, 
+# i.e., adds 1 to the 18. 
+# The return value is 19
 
